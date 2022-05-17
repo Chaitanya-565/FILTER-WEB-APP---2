aaf5cd1e2e42517753b4d910c0ecddea20ce8723
Chaitanya-565 <84910568+Chaitanya-565@users.noreply.github.com>
2022-05-17 15:02:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    save("my_picture.png");
+}
+
+function modelLoaded() {
+   console.log("poseNet is Intialised"); 
+}
+
+function gotPose(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("nose x: "+results[0].pose.nose.x);
+        console.log("nose y: "+results[0].pose.nose.y);
+    };
+}
